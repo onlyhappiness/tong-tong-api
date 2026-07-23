a58db62e07c1bbe61811ede3b9766810808f644b
onlyhappiness <onlyhappiness96@gmail.com>
2026-07-23 22:13:17 +0900

    feat: add PetService with hatch/feed/pet/release orchestration

--- src/modules/pet/pet.module.ts
+++ src/modules/pet/pet.module.ts
@@ -1,10 +1,14 @@
+import { User } from '@/modules/user/model/user.entity';
+import { Wallet } from '@/modules/user/model/wallet.entity';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PetState } from './model/pet-state.entity';
 import { Pet } from './model/pet.entity';
+import { PetService } from './pet.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Pet, PetState])],
-  exports: [TypeOrmModule],
+  imports: [TypeOrmModule.forFeature([Pet, PetState, Wallet, User])],
+  providers: [PetService],
+  exports: [PetService, TypeOrmModule],
 })
 export class PetModule {}
