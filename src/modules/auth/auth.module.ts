0db5769e2bd2b7adc0770b1212b7c5c8998a8cea
onlyhappiness <onlyhappiness96@gmail.com>
2026-07-23 23:08:07 +0900

    feat: provision wallet, daily limit, and starter egg on signup

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,7 +1,10 @@
 import { AuthGuard } from '@/common/guards/auth.guard';
+import { PetModule } from '@/modules/pet/pet.module';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DailyLimit } from '../user/model/daily-limit.entity';
 import { User } from '../user/model/user.entity';
+import { Wallet } from '../user/model/wallet.entity';
 import { UserModule } from '../user/user.module';
 import { AuthController } from './controller/auth.controller';
 import { Account } from './model/account.entity';
@@ -14,8 +17,16 @@ import { SessionService } from './service/session.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, Account, Session, Verification]),
+    TypeOrmModule.forFeature([
+      User,
+      Account,
+      Session,
+      Verification,
+      Wallet,
+      DailyLimit,
+    ]),
     UserModule,
+    PetModule,
   ],
   controllers: [AuthController],
   providers: [
